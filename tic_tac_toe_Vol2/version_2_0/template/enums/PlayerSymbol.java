c20ac5a93141c75c12805e4eb18c0274b2acf18a
Olga Iamshchikova <oljabotova@gmail.com>
2023-05-15 21:52:29 +0200

    Tic Tac Toe Version 2.0

---
+++ tic_tac_toe_Vol2/version_2_0/template/enums/PlayerSymbol.java
@@ -0,0 +1,46 @@
+package topics.course_projects.tic_tac_toe.version_2_0.template.enums;
+
+/**
+ * Enum of player symbols
+ * Перечисление символов игрока
+ */
+public enum PlayerSymbol {
+
+    /**
+     * Symbol X
+     * Символ X
+     */
+    X ('X'),
+
+    /**
+     * Symbol O
+     * Символ O
+     */
+    O ('O');
+
+    /**
+     * Symbol value
+     * Символьное значение
+     */
+    private final char VALUE;
+
+    /**
+     * Constructor
+     * Конструктор
+     *
+     * @param value symbol value / символьное значение
+     */
+    PlayerSymbol(char value) {
+        this.VALUE = value;
+    }
+
+    /**
+     * Getter
+     * Геттер
+     *
+     * @return symbol value / символьное значение
+     */
+    public char getValue() {
+        return VALUE;
+    }
+}
---
+++ tic_tac_toe_Vol2/version_2_0/template/interfaces/Player.java
@@ -0,0 +1,35 @@
+package topics.course_projects.tic_tac_toe.version_2_0.template.interfaces;
+
+import topics.course_projects.tic_tac_toe.version_2_0.template.enums.PlayerSymbol;
+
+/**
+ * Player
+ * Игрок
+ */
+public interface Player {
+
+    /**
+     * Getter
+     * Геттер
+     *
+     * @return player symbol / символ игрока
+     */
+    PlayerSymbol getSymbol();
+
+    /**
+     * Getter
+     * Геттер
+     *
+     * @return player name / имя игрока
+     */
+    String getName();
+
+    /**
+     * Make a move
+     * Сделать ход
+     *
+     * @return  coordinates in string format with a space as a splitter, example - "2 3"
+     *          координаты в виде строки с разделителем-пробелом, например - "2 3"
+     */
+    String makeMove();
+}
